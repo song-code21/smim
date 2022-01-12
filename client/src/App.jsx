14da17e4eec3fd09df8d39f231afab25338be9d0
song-code21 <wkcjf131313@gmail.com>
2022-01-12 23:03:31 +0900

    [SET] add pages/files from client
    클라이언트 기본 페이지 세팅

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,39 @@
 import React from "react";
+import Intro from "./pages/Intro";
+import MainPage from "./pages/mainPage";
+import SignupPage from "./pages/signupPage";
+import PostsPage from "./pages/PostsPage";
+import PostVeiw from "./pages/PostVeiw";
+import PostCreate from "./pages/PostCreate";
+import PostEdit from "./pages/postEdit";
+import MyPage from "./pages/myPage";
+import LoadingPage from "./pages/loadingPage";
+import NotFound from "./pages/NotFound";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-  return <div></div>;
+  return (
+    <>
+      <Routes>
+        <Route path="/intro" element={<Intro loginId={loginId} />} />
+        <Route path="/signup" element={<SignupPage loginId={loginId} />} />
+        <Route path="/" element={<MainPage loginId={loginId} />} />
+        <Route path="/posts" element={<PostsPage loginId={loginId} />} />
+        <Route
+          path="/posts/view/:id"
+          element={<PostVeiw loginId={loginId} />}
+        />
+        <Route
+          path="/posts/create"
+          element={<PostCreate loginId={loginId} />}
+        />
+        <Route path="/posts/edit" element={<PostEdit loginId={loginId} />} />
+        <Route path="/my" element={<MyPage loginId={loginId} />} />
+        <Route path="/loading" element={<LoadingPage loginId={loginId} />} />
+        <Route path="/notfound" element={<NotFound loginId={loginId} />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
