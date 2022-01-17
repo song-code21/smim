05523d3de9eb8aa62a0f75c5346eecfacd01e0b3
song-code21 <wkcjf131313@gmail.com>
2022-01-17 15:18:35 +0900

    [STYLE] Update Intro for layout
    인트로 페이지 레이아웃 컨테이너 추가

--- client/src/pages/Intro.jsx
+++ client/src/pages/Intro.jsx
@@ -1,5 +1,10 @@
 import React from "react";
+import styled from "styled-components";
+
+const IntroCtn = styled.div`
+  width: 1080px;
+`;
 
 export default function Intro() {
-  return <div></div>;
+  return <IntroCtn></IntroCtn>;
 }
