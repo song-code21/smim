e22819dc6d14bd1b4e03e2c5081880aa5234f790
song-code21 <wkcjf131313@gmail.com>
2022-01-18 17:56:23 +0900

    [STYLE] Update introPage for layout
    일러스트 추가, 기타 레이아웃 작업

--- client/src/styles/common/buttons.js
+++ client/src/styles/common/buttons.js
@@ -1,33 +1,30 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export const BorderBtn = styled.button`
-  width: ${({ width }) => width || "3.5rem"};
-  padding: ${({ padding }) => padding || ".2rem"};
+  width: ${({ width }) => width || '3.5rem'};
+  padding: ${({ padding }) => padding || '.2rem'};
   color: ${({ palette, theme }) => theme.color[palette]};
   border: 1.5px solid
-    ${({ palette, theme }) =>
-      palette ? theme.color[palette] : theme.color["black"]};
+    ${({ palette, theme }) => (palette ? theme.color[palette] : theme.color['black'])};
   &:hover {
     background-color: ${({ palette, theme }) =>
-      palette ? theme.color[palette] : theme.color["black"]};
+      palette ? theme.color[palette] : theme.color['black']};
     color: #fff;
     cursor: pointer;
   }
 `;
 
 export const ColorBtn = styled.button`
-  width: ${({ width }) => width || "3.5rem"};
-  padding: ${({ padding }) => padding || ".2rem"};
+  width: ${({ width }) => width || '3.5rem'};
+  height: ${({ height }) => height || '2rem'};
+  padding: ${({ padding }) => padding || '.2rem'};
   color: #fff;
   border: 1px solid
-    ${({ palette, theme }) =>
-      palette ? theme.color[palette] : theme.color["black"]};
-  background: ${({ palette, theme }) =>
-    palette ? theme.color[palette] : theme.color["black"]};
+    ${({ palette, theme }) => (palette ? theme.color[palette] : theme.color['black'])};
+  background: ${({ palette, theme }) => (palette ? theme.color[palette] : theme.color['black'])};
   &:hover {
     background: none;
-    color: ${({ palette, theme }) =>
-      palette ? theme.color[palette] : theme.color["black"]};
+    color: ${({ palette, theme }) => (palette ? theme.color[palette] : theme.color['black'])};
     cursor: pointer;
   }
 `;
